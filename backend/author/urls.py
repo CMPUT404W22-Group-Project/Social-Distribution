e4a3a15cea1c3a90b2774e68fd687ca2b080c84c
Shi Ning Le <71624746+Codyle212@users.noreply.github.com>
2022-02-15 15:59:27 -0700

    Delete backend directory

--- backend/author/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import AuthorListAPIView, AuthorDetailAPIView
-
-
-urlpatterns = [
-    path('', AuthorListAPIView.as_view(), name='authors'),
-    path('<author_id>/', AuthorDetailAPIView.as_view(), name='authorbyid'),
-]
