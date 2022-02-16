6f48db2a3b88bd4a60d1281eccc202615a5585e4
Shi Ning Le <shining@ualberta.ca>
2022-02-15 18:59:44 -0700

    express backend boiler plate

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+//routes
+
+//static files
+//use this as the last routes, else it might interfere with other routes used for depolyment
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+// });
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}... `);
+});
