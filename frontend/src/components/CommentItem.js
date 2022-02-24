2c34b9cea7c5b15a8517cdc8ceeb2bb8e7ae920b
Shi Ning Le <shining@ualberta.ca>
2022-02-24 16:41:09 -0700

    add routing (partial)

--- frontend/src/components/CommentItem.js
+++ frontend/src/components/CommentItem.js
@@ -8,7 +8,6 @@ import CardActions from '@mui/material/CardActions';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
 
 import ProfilePictureCard from './ProfilePictureCard';
 
@@ -45,9 +44,6 @@ const CommentItem = ({ props }) => {
         <CardActions disableSpacing>
           <IconButton aria-label="add to favorites">
             <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="share">
-            <ShareIcon />
           </IconButton>
         </CardActions>
       </Card>
