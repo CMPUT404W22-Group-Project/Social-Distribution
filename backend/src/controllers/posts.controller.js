ebfd13dd22094e4209b2598fb2b24bd65121bc49
Nathan <nathan.ly.tang@gmail.com>
2022-02-26 18:26:13 -0700

    Fix: Use cuid to generate id for POST

--- backend/src/controllers/posts.controller.js
+++ backend/src/controllers/posts.controller.js
@@ -1,14 +1,26 @@
 import { postService, authorService } from "../services/index.service.js";
+import cuid from "cuid";
 
-export async function httpGetAllPosts(req, res) {
-    const posts = await postService.getPosts({ authorid: parseInt(req.params.authorid), page: parseInt(req.query.page), size: parseInt(req.query.size) });
+/**
+ * Get all posts of a given author
+ * @param {Express.Request} req 
+ * @param {Express.Response} res 
+ * @returns All the posts of an author
+ */
+export async function getAllPosts(req, res) {
+    const posts = await postService.getPosts({ authorId: parseInt(req.params.authorId), page: parseInt(req.query.page), size: parseInt(req.query.size) });
     const host = `${req.protocol}://${req.get('host')}`;
+    const author = await authorService.getAuthors({ id: req.params.authorId });
+    if (!author) {
+        return res.status(404).json({ error: "Author Not Found" });
+    }
 
     posts.forEach(post => {
         post.type = "post";
         post.url = `${host}/authors/${post.authorId}/posts/${post.id}`;
         post.id = `${host}/authors/${post.authorId}/posts/${post.id}`;
         post.host = `${host}/`;
+        post.author = { type: "author", ...author };
     });
 
     const response = {
@@ -19,15 +31,24 @@ export async function httpGetAllPosts(req, res) {
     return res.status(200).json(response);
 }
 
-export async function httpGetOnePost(req, res) {
+/**
+ * Get one post of a given author (using author id)
+ * @param {Express.Request} req 
+ * @param {Express.Response} res 
+ * @returns The post information requested
+ */
+export async function getOnePost(req, res) {
     const post = await postService.getPosts({ id: req.params.id });
     const host = `${req.protocol}://${req.get('host')}`;
-    
     if (!post) {
-        return res.status(404).json({ error: "Not Found" });
+        return res.status(404).json({ error: "Post Not Found" });
     }
-    
+
     const author = await authorService.getAuthors({ id: post.authorId });
+    if (!author) {
+        return res.status(404).json({ error: "Author Not Found" });
+    }
+
     post.url = `${host}/authors/${post.authorId}/posts/${post.id}`;
     post.id = `${host}/authors/${post.authorId}/posts/${post.id}`;
     post.host = `${host}/`;
@@ -41,48 +62,88 @@ export async function httpGetOnePost(req, res) {
     return res.status(200).json(response);
 }
 
-export async function httpPutPost(req, res) {
+/**
+ * Create a new post with a specified post ID
+ * @param {Express.Request} req 
+ * @param {Express.Response} res 
+ * @returns New post
+ */
+export async function putPost(req, res) {
     const post = req.body;
-    if (
-        !post.author ||
-        !post.authorId ||
-        !post.title ||
-        !post.content ||
-        !post.visibility
-    ) {
-        return res.status(400).json({
-            error: 'Missing required property',
-        });
-    }
+    post.id = req.params.id;
 
-    const newPost = await postService.postPost(post);
+    if (!cuid.isCuid(req.params.id)) return res.status(400).json({ error: "Invalid post id" });
+    if (!validPost(post)) return res.status(400).json({ error: 'Missing required property' });
+
+    const newPost = await postService.putPost(post);
     return res.status(201).json(newPost);
 }
 
-export async function httpDeletePost(req, res) {
-    const id = req.body.id;
+/**
+ * Delete a post given the post ID
+ * @param {Express.Request} req 
+ * @param {Express.Response} res 
+ * @returns Deleted post
+ */
+export async function deletePost(req, res) {
+    const id = req.params.id;
     if (!id) {
-        return res.status(400).json({
-            error: 'Missing required property',
-        });
+        return res.status(400).json({ error: 'Missing required property' });
     }
     const deletedPost = await postService.deletePost(id);
     return res.status(204).json(deletedPost);
 }
 
-export async function httpUpdatePost(req, res) {
+/**
+ * Update a post given the post ID
+ * @param {Express.Request} req 
+ * @param {Express.Response} res 
+ * @returns Updated post
+ */
+export async function updatePost(req, res) {
     const post = req.body;
-    if (
-        !post.author ||
+    post.id = req.params.id;
+
+    if (!validPost(post)) {
+        return res.status(400).json({ error: 'Missing required property' });
+    }
+
+    const updatedPost = await postService.updatePost(post);
+    return res.status(200).json(updatedPost);
+}
+
+/**
+ * Create a new post with a generated ID
+ * @param {Express.Request} req 
+ * @param {Express.Response} res 
+ * @returns New post
+ */
+export async function newPost(req, res) {
+    const post = req.body;
+    post.id = cuid();
+    if (!validPost(post)) return res.status(400).json({ error: 'Missing required property' });
+
+    const newPost = await postService.newPost(post);
+    return res.status(201).json(newPost);
+}
+
+/**
+ * Check if post has all the required arguments
+ * @param {Post object} post 
+ * @returns Boolean
+ */
+function validPost(post) {
+    if (!post.id ||
         !post.authorId ||
         !post.title ||
-        !post.content ||
-        !post.visibility
-    ) {
-        return res.status(400).json({
-            error: 'Missing required property',
-        });
+        !post.source ||
+        !post.origin ||
+        !post.description ||
+        !post.contentType ||
+        !post.published ||
+        !post.visibility ||
+        post.unlisted === undefined) {
+        return false;
     }
-    const newPost = await postService.updatePost(post);
-    return res.status(201).json(newPost);
+    return true;
 }
