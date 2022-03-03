ca614625ef51690700c801ec063e249e89679e6c
Nathan Tang <nathan.ly.tang@gmail.com>
2022-03-02 20:01:04 -0700

    Fix: Remove /service/ from route urls

--- backend/src/routes/authors.router.js
+++ backend/src/routes/authors.router.js
@@ -3,7 +3,7 @@ import * as authorController from "../controllers/authors.controller.js";
 
 const router = Router();
 
-router.get('/service/authors/', authorController.getAllAuthors);
-router.get('/service/authors/:id', authorController.getOneAuthor);
+router.get('/authors/', authorController.getAllAuthors);
+router.get('/authors/:id', authorController.getOneAuthor);
 
 export { router };
--- backend/src/routes/posts.router.js
+++ backend/src/routes/posts.router.js
@@ -3,11 +3,11 @@ import * as postController from "../controllers/posts.controller.js";
 
 const router = Router();
 
-router.get('/service/authors/:authorId/posts/', postController.getAllPosts);
-router.get('/service/authors/:authorId/posts/:id', postController.getOnePost);
+router.get('/authors/:authorId/posts/', postController.getAllPosts);
+router.get('/authors/:authorId/posts/:id', postController.getOnePost);
 
-router.put('/service/authors/:authorId/posts/:id', postController.putPost);
-router.delete('/service/authors/:authorId/posts/:id', postController.deletePost);
-router.post('/service/authors/:authorId/posts/:id', postController.updatePost);
-router.post('/service/authors/:authorId/posts/', postController.newPost);
+router.put('/authors/:authorId/posts/:id', postController.putPost);
+router.delete('/authors/:authorId/posts/:id', postController.deletePost);
+router.post('/authors/:authorId/posts/:id', postController.updatePost);
+router.post('/authors/:authorId/posts/', postController.newPost);
 export { router };
