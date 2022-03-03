ca1e3ffeb161837b537bdbfad38a3f7a62c142e2
Nathan Tang <nathan.ly.tang@gmail.com>
2022-03-03 02:06:49 -0700

    Fix: remove /service/

--- backend/src/routes/likes.router.js
+++ backend/src/routes/likes.router.js
@@ -3,9 +3,9 @@ import * as likeController from "../controllers/likes.controller.js";
 
 const router = Router();
 
-router.post('/service/authors/:authorId/inbox/', likeController.httpPostNewLike);
-router.get('/service/authors/:authorId/posts/:postId/likes/', likeController.httpGetAllLikesOfPost);
-router.get('/service/authors/:authorId/posts/:postId/comments/:commentId/likes', likeController.httpGetAllLikesOfComment);
-router.get('/service/authors/:authorId/liked')
+router.post('/authors/:authorId/inbox/', likeController.httpPostNewLike);
+router.get('/authors/:authorId/posts/:postId/likes/', likeController.httpGetAllLikesOfPost);
+router.get('/authors/:authorId/posts/:postId/comments/:commentId/likes', likeController.httpGetAllLikesOfComment);
+router.get('/authors/:authorId/liked');
 
 export { router };
