f5a998fa12d05f79b0cb2688a6bbf012348c42fa
Nathan Tang <nathan.ly.tang@gmail.com>
2022-03-03 01:46:44 -0700

    Feat: Get comments with author information sorted

--- backend/src/services/comments.service.js
+++ backend/src/services/comments.service.js
@@ -2,41 +2,68 @@ import PrismaClient from "@prisma/client";
 
 const prisma = new PrismaClient.PrismaClient();
 
-export async function getComments(options){
-    const {postid, id, page, size} = options;
+export async function getComments(options) {
+    const { postId, id, page, size } = options;
 
-    if (id){
+    if (id) {
         return await prisma.comment.findUnique({
-            where:{
-                id:id
+            where: {
+                id: id
             }
         });
     }
 
-    if (page && size){
+    if (page && size) {
         return await prisma.comment.findMany({
-          where:{
-            postid: postid,
+            where: {
+                postId: postId,
+            },
             skip: size * (page - 1),
-            take: size
-          }
+            take: size,
+            include: {
+                author: {
+                    select: {
+                        displayName: true,
+                        github: true,
+                        profileImage: true
+                    }
+                }
+            },
+            orderBy: {
+                published: 'desc'
+            }
         });
     }
 
-    return await prisma.comment.findMany();
+    return await prisma.comment.findMany({
+        where: {
+            postId: postId
+        },
+        include: {
+            author: {
+                select: {
+                    displayName: true,
+                    github: true,
+                    profileImage: true
+                }
+            }
+        },
+        orderBy: {
+            published: 'desc'
+        }
+    });
 }
 
-export async function postComment(comment){
+export async function newComment(comment) {
     return await prisma.comment.create({
         data: {
-          author: comment.author,
-          authorId: comment.authorId,
-          post:comment.post,
-          postId:comment.postId,
-          contentType: comment.contentType,
-          comment: comment.comment,
-          published: comment.published,
+            id: comment.id,
+            authorId: comment.authorId,
+            postId: comment.postId,
+            contentType: comment.contentType,
+            comment: comment.comment,
+            published: new Date(comment.published),
         },
-      })
-    
+    });
+
 }
