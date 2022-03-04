0ebb832cbbf574aff92e209c6405120367e9ace7
Shi Ning Le <shining@ualberta.ca>
2022-03-03 23:30:19 -0700

    1

---
+++ frontend/src/containers/Post/AuthorPost.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import PostItem from '../../components/PostItem';
+
+const BACKEND_URL = 'http://localhost:8000'; //process.env.REACT_APP_BACKEND_URL
+
+const AuthorPost = ({ props }) => {
+    AuthorPost.propTypes = {
+        props: PropTypes.object,
+        auth: PropTypes.object,
+    };
+    const [posts, setPosts] = useState([]);
+    let { authorId } = useParams();
+    const isOwnPost = props.auth.author.id === authorId;
+    let navigate = useNavigate();
+    const getAllPosts = () => {
+        axios
+            .get(`${BACKEND_URL}/authors/${authorId}/posts`)
+            .then((response) => {
+                response.status === 200
+                    ? setPosts([...response.data.items])
+                    : null;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+    useEffect(() => {
+        getAllPosts();
+    }, []);
+    return (
+        <>
+            {isOwnPost ? (
+                <Button
+                    sx={{
+                        minWidth: 100,
+                        my: 5,
+                        left: '80%',
+                        position: 'relative',
+                    }}
+                    variant="contained"
+                    onClick={() => {
+                        navigate(`/authors/${authorId}/post/new`);
+                    }}
+                >
+                    New Post
+                </Button>
+            ) : null}
+            {posts?.map((post, i) => {
+                return post !== undefined ? (
+                    <PostItem key={i} props={post} />
+                ) : null;
+            })}
+        </>
+    );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+    props: { ...ownProps.props, auth: state.auth },
+});
+
+export default connect(mapStateToProps)(AuthorPost);
