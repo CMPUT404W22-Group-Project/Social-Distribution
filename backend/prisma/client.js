87b04cf608030ae541804743a92d907a29d37e0b
Nathan Tang <nathan.ly.tang@gmail.com>
2022-03-03 22:43:19 -0700

    Fix: Change Prisma import depending on dev or test

--- backend/prisma/client.js
+++ backend/prisma/client.js
@@ -1,4 +1,13 @@
-import prismaClient from '@prisma/client';
+import Prisma from '@prisma/client';
+let prisma;
 
-const prisma = new prismaClient.PrismaClient();
+try {
+    // When app is run in dev
+	prisma = new Prisma.PrismaClient();
+} catch {
+    // When app is run in test
+	const Prisma = require('@prisma/client');
+	prisma = new Prisma.PrismaClient();
+}
+
 export default prisma;
