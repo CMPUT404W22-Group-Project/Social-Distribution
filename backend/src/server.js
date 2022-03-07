942e6d86ff51fe85042d111001afbaca412b366d
Nathan Tang <nathan.ly.tang@gmail.com>
2022-03-06 18:51:33 -0700

    Fix: Require cookieparser

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import fileUpload from 'express-fileupload';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import router from './routes/index.router.js';
 dotenv.config();
+import router from './routes/index.router.js';
 const app = express();
 
 const PORT = process.env.PORT || 8000;
@@ -14,6 +15,7 @@ app.use(
 );
 app.use(express.json());
 app.use(fileUpload());
+app.use(cookieParser());
 router(app);
 
 //use this as the last routes, else it might interfere with other routes used for depolyment
