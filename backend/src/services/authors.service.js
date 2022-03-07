88602c65426fd072712ade25e967abdb725accc3
Nathan Tang <nathan.ly.tang@gmail.com>
2022-03-06 17:25:34 -0700

    Upsert only if parameters exist, check if user exists

--- backend/src/services/authors.service.js
+++ backend/src/services/authors.service.js
@@ -26,7 +26,7 @@ export async function getAuthors(options) {
 /**
  * post a author to database, if it already
  * exist, then update it.
- * @param {author} author
+ * @param {Author object} author
  * @returns
  */
 export async function newAuthor(author) {
@@ -35,21 +35,36 @@ export async function newAuthor(author) {
 			id: author.id,
 		},
 		update: {
-			displayName: author.displayName,
-			github: author.github,
-			profileImage: author.profileImage,
-			Post: author.Post,
-			Comment: author.Comment,
-			Likes: author.Comment,
+			email: author.email != null ? author.email : undefined,
+			password: author.password != null ? author.password : undefined,
+			displayName: author.displayName != null ? author.displayName : undefined,
+			github: author.github != null ? author.github : undefined,
+			profileImage: author.profileImage != null ? author.profileImage : undefined,
 		},
 		create: {
-			id: author.id,
-			displayName: author.displayName,
-			github: author.github,
-			profileImage: author.profileImage,
-			Post: author.Post,
-			Comment: author.Comment,
-			Likes: author.Comment,
+			id: author.id, // Required
+			email: author.email, // Required
+			password: author.password, // Required
+			displayName: author.displayName != null ? author.displayName : undefined,
+			github: author.github != null ? author.github : undefined,
+			profileImage: author.profileImage != null ? author.profileImage : undefined,
 		},
+	});
+}
+
+/**
+ * Check if user email already exists in the database
+ * @param {String} email
+ * @returns email
+ */
+export async function checkUserExists(email) {
+	return await prisma.author.findFirst({
+		where: {
+			email: email,
+		},
+		select: {
+			email: true,
+			id: true
+		}
 	});
 }
