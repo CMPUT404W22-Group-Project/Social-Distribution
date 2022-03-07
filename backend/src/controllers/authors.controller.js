fc095a0ed4bc1bdc7fa93ddb22a57fa77f22bce1
Nathan Tang <nathan.ly.tang@gmail.com>
2022-03-06 17:26:30 -0700

    Feat: Register, login, logout using JWT & cookies

--- backend/src/controllers/authors.controller.js
+++ backend/src/controllers/authors.controller.js
@@ -1,4 +1,7 @@
 import { authorService } from '../services/index.service.js';
+import { authenticatePassword, newAccessToken } from '../auth/index.js';
+import cuid from 'cuid';
+import argon2 from 'argon2';
 
 /**
  * Get all the authors of a given host
@@ -48,23 +51,81 @@ export async function getOneAuthor(req, res) {
 	return res.status(200).json(response);
 }
 
-export async function postAuthor(req, res) {
-	const author = req.body;
-	author.id = req.params.id;
-	if (!validAuthor(author)) {
+/**
+ * Create a new author/user
+ * @param {Express.Request} req
+ * @param {Express.Response} res
+ * @returns Author information
+ */
+export async function newAuthor(req, res) {
+	const user = req.body;
+	user.id = cuid();
+
+	// Check if all the required parameters exist
+	if (!validAuthor(user)) {
 		return res.status(400).json({ error: 'Missing required property' });
 	}
-	const newAuthor = await authorService.newAuthor(author);
-	return res.status(201).json(newAuthor);
+
+	// Check if the user already exists
+	if (await authorService.checkUserExists(user.email)) {
+		return res.status(409).json({ error: 'Email already in use' });
+	}
+
+	// Hash the password and store the new user in the database
+	user.password = await argon2.hash(user.password);
+	const newUser = await authorService.newAuthor(user);
+
+	return res.status(201).json(newUser);
 }
 
+/**
+ * Login a user by email and password
+ * @param {Express.Request} req
+ * @param {Express.Response} res
+ * @returns Access token cookie
+ */
+export async function login(req, res) {
+	const user = req.body;
+
+	// Check if the user email exists
+	const userExists = await authorService.checkUserExists(user.email);
+	if (!userExists) {
+		return res.status(409).json({ error: 'User does not exist' });
+	} else {
+		user.id = userExists.id;
+	}
+
+	// Check if the user password matches
+	if (!(await authenticatePassword(user.email, user.password))) {
+		return res.status(401).json({ error: 'Password mismatch' });
+	}
+
+	// Create new JWT token and set it as a cookie
+	const token = await newAccessToken(user.email, user.id);
+	res.cookie('TOKEN', token, { maxAge: 604800, sameSite: 'strict' }); // 7 days
+	return res.status(200).json({ token: token });
+}
+
+/**
+ * Clear access token and logout
+ * @param {Express.Request} req
+ * @param {Express.Response} res
+ * @returns 401
+ */
+export async function logout(req, res) {
+	if (req.cookies.TOKEN) {
+		res.clearCookie('TOKEN', { sameSite: true });
+	}
+	return res.sendStatus(401);
+}
+
+/**
+ * Check if valid author parameters exist
+ * @param {Author object} author
+ * @returns Boolean
+ */
 function validAuthor(author) {
-	if (
-		!author.id ||
-		!author.github ||
-		!author.displayName ||
-		!author.profileImage
-	) {
+	if (!author.id || !author.email || !author.password || !author.displayName) {
 		return false;
 	}
 	return true;
