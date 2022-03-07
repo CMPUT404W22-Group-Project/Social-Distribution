471e9d225ec960f8bc4b767b119ce87efc9a0ca4
Nathan Tang <nathan.ly.tang@gmail.com>
2022-03-07 14:22:56 -0700

    Feat: Update user information

--- backend/src/routes/authors.router.js
+++ backend/src/routes/authors.router.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import * as authorController from '../controllers/authors.controller.js';
+import { authenticateToken } from '../auth/index.js';
 
 const router = Router();
 
 router.get('/authors/', authorController.getAllAuthors);
 router.get('/authors/:id', authorController.getOneAuthor);
+router.post('/authors/:id', authenticateToken, authorController.updateAuthor);
 router.post('/register', authorController.newAuthor);
 router.post('/login', authorController.login);
 router.get('/logout', authorController.logout);
