bc57306558c208eeb5335c11bdfa91b501f64832
Nathan Tang <nathan.ly.tang@gmail.com>
2022-03-21 21:28:22 -0600

    Add likedId field for inbox likes

---
+++ backend/prisma/migrations/20220322010122_fix_remove_follow_id_from_inbox/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `followId` on the `Inbox` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Inbox" DROP COLUMN "followId";
