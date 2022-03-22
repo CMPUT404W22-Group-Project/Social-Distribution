e6364e162754251c6cb021b6d3d89f98f29e16e3
Nathan Tang <nathan.ly.tang@gmail.com>
2022-03-21 21:28:49 -0600

    Ref: Use proper Prisma client

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,6 +1,4 @@
-import PrismaClient from '@prisma/client';
-
-const prisma = new PrismaClient.PrismaClient();
+import prisma from '../../prisma/client.js';
 
 export async function getHashedPassword(email) {
 	return await prisma.author.findUnique({
