84a35dd2ec0aa09a174aed310f645f2b425b6643
Andyxzz2 <61675598+Andyxzz2@users.noreply.github.com>
2022-03-23 00:09:03 -0600

    api-doc template

--- backend/src/routes/authors.router.js
+++ backend/src/routes/authors.router.js
@@ -4,6 +4,32 @@ import { authenticateToken } from '../auth/index.js';
 
 const router = Router();
 
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         title:
+ *           type: string
+ *           description: The book title
+ *         author:
+ *           type: string
+ *           description: The book author
+ *       example:
+ *         id: d5fE_asz
+ *         title: The New Turing Omnibus
+ *         author: Alexander K. Dewdney
+ */
+
 router.get('/authors/', authorController.getAllAuthors);
 router.get('/authors/:id', authorController.getOneAuthor);
 router.post('/authors/:id', authenticateToken, authorController.updateAuthor);
--- backend/src/routes/index.router.js
+++ backend/src/routes/index.router.js
@@ -3,6 +3,8 @@ import { router as posts } from './posts.router.js';
 import { router as comment } from './comments.router.js';
 import { router as likes } from './likes.router.js';
 
+
+
 export default function (app) {
 	app.use('/', author);
 	app.use('/', posts);
