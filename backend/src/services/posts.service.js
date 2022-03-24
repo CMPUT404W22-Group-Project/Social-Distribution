b0adcd6d0de9911ac6feb9f290294e7523bb600f
Nathan Tang <nathan.ly.tang@gmail.com>
2022-03-24 02:35:04 -0600

    Fix: Do not get unlisted

--- backend/src/services/posts.service.js
+++ backend/src/services/posts.service.js
@@ -19,6 +19,7 @@ export async function getPosts(options) {
 		return await prisma.post.findMany({
 			where: {
 				authorId: authorId,
+				unlisted: false,
 			},
 			skip: size * (page - 1),
 			take: size,
@@ -29,6 +30,10 @@ export async function getPosts(options) {
 	}
 
 	return await prisma.post.findMany({
+		where: {
+			authorId: authorId,
+			unlisted: false,
+		},
 		orderBy: {
 			published: 'desc',
 		},
