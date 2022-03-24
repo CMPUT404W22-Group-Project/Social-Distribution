4277888d66e12f080f3076f3d7cdea84e2ae32a7
Nathan Tang <nathan.ly.tang@gmail.com>
2022-03-24 16:18:55 -0600

    TODO: Fix likes

--- backend/src/routes/inbox.router.js
+++ backend/src/routes/inbox.router.js
@@ -5,6 +5,8 @@ import { authenticateToken } from '../auth/index.js';
 const router = Router();
 
 router.get('/authors/:id/inbox', authenticateToken, inboxController.getInbox);
+// TODO: Basic auth
+router.post('/authors/:id/inbox', authenticateToken, inboxController.postToInbox);
 router.delete('/authors/:id/inbox', authenticateToken, inboxController.deleteInbox);
 
 export { router };
--- backend/src/routes/likes.router.js
+++ backend/src/routes/likes.router.js
@@ -3,6 +3,7 @@ import * as likeController from '../controllers/likes.controller.js';
 
 const router = Router();
 
+// FIXME: Post route should be inbox, not like
 router.post('/authors/:authorId/posts/:postId/likes', likeController.httpPostNewLikeToPost);
 router.get(
 	'/authors/:authorId/posts/:postId/likes/',
