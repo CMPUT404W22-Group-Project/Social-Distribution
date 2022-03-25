272465bab349661e11ab83629879c1fe0b3b049e
Andyxzz2 <61675598+Andyxzz2@users.noreply.github.com>
2022-03-25 15:14:59 -0600

    fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import router from './routes/index.router.js';
+const cookieParser = require('cookie-parser');
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
 
 router(app);
 
