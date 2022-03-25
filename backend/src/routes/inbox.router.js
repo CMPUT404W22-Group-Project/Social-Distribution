fe90d39d0a497d56037372c17344e78671719aa3
Andyxzz2 <61675598+Andyxzz2@users.noreply.github.com>
2022-03-24 21:45:59 -0600

    new docs

--- backend/src/routes/inbox.router.js
+++ backend/src/routes/inbox.router.js
@@ -4,9 +4,136 @@ import { authenticateToken } from '../auth/index.js';
 
 const router = Router();
 
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Inbox:
+ *       type: object
+ *       required:
+ *         - type
+ *         - src
+ *         - owner
+ *         - author
+ *       properties:
+ *         id:
+ *           type: object
+ *           description: The combined id of src and owner
+ *         type:
+ *           type: string
+ *           description: The type of this element
+ *         src:
+ *           type: string
+ *           description: If post, then post id, If following, then following author id If like, then liked post id.  If comment, then comment id.  (URL if remote)
+ *         likedAuthor:
+ *           type: string
+ *           description: Contains the author id that liked the post.
+ *         owner:
+ *           type: string
+ *           description: the woner of the inbox
+ *         dateTime:
+ *           type: DayTime
+ *           description: Timestamp the event
+ *         author:
+ *           type: Author
+ *           description: the relative author
+ *       example:
+ *         type: post
+ *         src: dfsE_asz
+ *         likedAuthor: d5fsE_asz
+ *         owner: d5fsE_asz
+ *         dateTime: 2022-03-22
+ *         author: [dfsE_asz, d5fsE_asz]
+ *         id: {}
+ *     Inboxs:
+ *       type: object
+ *       properties:
+ *         items:
+ *           type: Object
+ *           description: The list of elements
+ *       example:
+ *         type: Object
+ *         items: []
+ */
+
+
+ /**
+  * @swagger
+  * tags:
+  *   name: Likes
+  *   description: The inbox managing API
+  */
+
+
+/**
+ * @swagger
+ * /authors/:id/inbox:
+ *   get:
+ *     summary: Returns the inbox
+ *     tags: [Inbox]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: int
+ *         required: true
+ *         description: id of the author
+ *     responses:
+ *       200:
+ *         description: the inbox found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               items:
+ *                 $ref: '#/components/schemas/Inboxs'
+ */
 router.get('/authors/:id/inbox', authenticateToken, inboxController.getInbox);
 // TODO: Basic auth
+
+
+/**
+ * @swagger
+ * /authors/:id/inbox:
+ *   post:
+ *     summary: Returns the inbox
+ *     tags: [Inbox]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: int
+ *         required: true
+ *         description: id of the author
+ *     responses:
+ *       201:
+ *         description: the element posted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               items:
+ *                 $ref: '#/components/schemas/Inbox'
+ */
 router.post('/authors/:id/inbox', authenticateToken, inboxController.postToInbox);
+
+
+/**
+ * @swagger
+ * /authors/:id/inbox:
+ *   delete:
+ *     summary: Returns the inbox
+ *     tags: [Inbox]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: int
+ *         required: true
+ *         description: id of the author
+ *     responses:
+ *       204:
+ *         description: the element deleted
+ */
 router.delete('/authors/:id/inbox', authenticateToken, inboxController.deleteInbox);
 
 export { router };
--- backend/src/routes/likes.router.js
+++ backend/src/routes/likes.router.js
@@ -3,12 +3,103 @@ import * as likeController from '../controllers/likes.controller.js';
 
 const router = Router();
 
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Like:
+ *       type: object
+ *       required:
+ *         - id
+ *         - postId
+ *         - authorId
+ *         - post
+ *         - author
+ *       properties:
+ *         id:
+ *           type: object
+ *           description: The combination of post id and author id
+ *         postId:
+ *           type: string
+ *           description: the post id of relative post
+ *         authorId:
+ *           type: string
+ *           description: The author id of relative author
+ *         post:
+ *           type: Post
+ *           description: the relative Post
+ *         author:
+ *           type: Author
+ *           description: The relative author
+
+ *       example:
+ *         id: [dfsE_asz,d5fsE_asz]
+ *         postId: dfsE_asz
+ *         authorId: d5fsE_asz
+ *         post: {}
+ *         author: {}
+ *     Likes:
+ *       type: object
+ *       properties:
+ *         items:
+ *           type: Like
+ *           description: The list of likes
+ *       example:
+ *         type: Like
+ *         items: []
+ */
+
+
+ /**
+  * @swagger
+  * tags:
+  *   name: Likes
+  *   description: The author managing API
+  */
+
+
 // FIXME: Post route should be inbox, not like
 router.post('/authors/:authorId/posts/:postId/likes', likeController.httpPostNewLikeToPost);
+
+
+
+/**
+ * @swagger
+ * /authors/:authorId/posts/:postId/likes/:
+ *   get:
+ *     summary: Returns all likes of a post
+ *     tags: [Likes]
+ *     responses:
+ *       200:
+ *         description: likes received
+ *         content:
+ *           application/json:
+ *             schema:
+ *               items:
+ *                 $ref: '#/components/schemas/Likes'
+ */
 router.get(
 	'/authors/:authorId/posts/:postId/likes/',
 	likeController.httpGetAllLikesOfPost
 );
+
+
+/**
+ * @swagger
+ * /authors/:authorId/posts/:postId/comments/:commentId/likes:
+ *   get:
+ *     summary: Returns all likes of a comment
+ *     tags: [Likes]
+ *     responses:
+ *       200:
+ *         description: likes received
+ *         content:
+ *           application/json:
+ *             schema:
+ *               items:
+ *                 $ref: '#/components/schemas/Likes'
+ */
 router.get(
 	'/authors/:authorId/posts/:postId/comments/:commentId/likes',
 	likeController.httpGetAllLikesOfComment
