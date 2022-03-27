bdd1fc5a405ee43baeb92384d5fd5cb68a33f80f
Nathan Tang <nathan.ly.tang@gmail.com>
2022-03-26 18:58:45 -0600

    Feat: Serve React build

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,32 +6,35 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'LogRocket Express API with Swagger',
-            version: '0.1.0',
-            description:
-                'This is a simple CRUD API application made with Express and documented with Swagger',
-            contact: {
-                name: 'LogRocket',
-                url: 'http://localhost:3000/',
-                email: 'shining@ualberta.ca',
-            },
-        },
-        servers: [
-            {
-                url: 'http://localhost:8000/',
-            },
-        ],
-    },
-    apis: ['./src/routes/*.router.js'],
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'LogRocket Express API with Swagger',
+			version: '0.1.0',
+			description:
+				'This is a simple CRUD API application made with Express and documented with Swagger',
+			contact: {
+				name: 'LogRocket',
+				url: 'http://localhost:3000/',
+				email: 'shining@ualberta.ca',
+			},
+		},
+		servers: [
+			{
+				url: 'http://localhost:8000/',
+			},
+		],
+	},
+	apis: ['./src/routes/*.router.js'],
 };
 
 const specs = swaggerJsdoc(options);
@@ -45,13 +48,12 @@ app.use(
 	})
 );
 
-
-
 app.use(express.json());
 app.use(fileUpload());
 app.use(cookieParser());
-
 router(app);
+
+app.use(express.static(path.join(__dirname, '../build')));
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}... `);
 });
