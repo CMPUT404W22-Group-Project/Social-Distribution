14af89e25ff91c3178beac157d79ff79bc7ec9fb
Shi Ning Le <shining@ualberta.ca>
2022-04-02 20:43:10 -0600

    Update Inbox.js

--- frontend/src/containers/Inbox/Inbox.js
+++ frontend/src/containers/Inbox/Inbox.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import List from '@mui/material/List';
+import Button from '@mui/material/Button';
+import InboxItem from '../../components/InboxItem';
 
 const Inbox = ({ props }) => {
     Inbox.propTypes = {
@@ -24,7 +27,34 @@ const Inbox = ({ props }) => {
         getLikes(authorId);
     }, [getLikes, authorId]);
 
-    return <div>Inbox</div>;
+    return (
+        <>
+            <Button
+                sx={{
+                    minWidth: 100,
+                    my: 5,
+                    left: '80%',
+                    position: 'relative',
+                }}
+                variant="contained"
+                onClick={() => {
+                    null;
+                }}
+            >
+                Clear
+            </Button>
+            <List
+                sx={{
+                    maxWidth: 4 / 5,
+                    mt: 10,
+                }}
+            >
+                {inbox?.map((item, i) => {
+                    return <InboxItem key={i} props={item} />;
+                })}
+            </List>
+        </>
+    );
 };
 
 const mapStateToProps = (state, ownProps) => ({
