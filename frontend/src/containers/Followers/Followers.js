2f5fca696a102164bcce928ddc5b2a637acc609b
Nathan Tang <nathan.ly.tang@gmail.com>
2022-04-03 21:47:45 -0600

    Feat: Show followers

--- frontend/src/containers/Followers/Followers.js
+++ frontend/src/containers/Followers/Followers.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import RemoteProfile from "../../remoteComponents/RemoteProfile";
+import axios from "axios";
 
 const Followers = () => {
-    return <div>Followers</div>;
+    useEffect(() => {
+        document.title = "Followers";
+    }, []);
+
+    let { authorId } = useParams();
+
+    const [followers, setFollowers] = useState([]);
+
+    const getFollowers = useCallback(async () => {
+        const response = await axios.get(`/authors/${authorId}/followers`);
+        setFollowers(response.data.items);
+    }, [authorId]);
+
+    useEffect(() => {
+        getFollowers();
+    }, [getFollowers]);
+
+    return (
+        <Grid container spacing={2}>
+            {followers?.map((follower, index) => {
+                return follower ? (
+                    <Grid key={index} item xs={1} sx={{ mx: 1, my: 1 }}>
+                        <RemoteProfile props={follower} />
+                    </Grid>
+                ) : null;
+            })}
+        </Grid>
+    );
 };
 
 export default Followers;
